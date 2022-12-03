ccf55a49d4af24bbbf04d259ec7fcb722e18b7e8
WeepEagle <276363144@qq.com>
2022-12-04 01:20:41 +0800

    123

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -6,19 +6,19 @@ export default [
   {
     name: 'room',
     type: 'glbModel',
-    path: '/models/MyHome.glb',
+    path: '/myHome/models/MyHome.glb',
     // path: './MyHome.glb',
   },
   {
     name: 'screen',
     type: 'videoTexture',
-    path: '/textures/miao.mp4',
+    path: '/myHome/textures/miao.mp4',
     // path: './miao.mp4',
   },
   {
     name: 'window',
     type: 'videoTexture',
-    path: '/textures/miao.mp4',
+    path: '/myHome/textures/miao.mp4',
     // path: './miao.mp4',
   },
 ]
