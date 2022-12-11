40302dfe7e05fa095dffc8f53d97e627c1c13d6f
WeepEagle <276363144@qq.com>
2022-12-11 11:50:04 +0800

    1232131

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -7,18 +7,18 @@ export default [
     name: 'room',
     type: 'glbModel',
     // path: '/my-home/models/MyHome.glb',
-    path: './MyHome.glb',
+    path: '/models/MyHome.glb',
   },
   {
     name: 'screen',
     type: 'videoTexture',
     // path: '/my-home/textures/miao.mp4',
-    path: './miao.mp4',
+    path: '/textures/miao.mp4',
   },
   {
     name: 'window',
     type: 'videoTexture',
     // path: '/my-home/textures/miao.mp4',
-    path: './miao.mp4',
+    path: '/textures/miao.mp4',
   },
 ]
